aba6e555a10cc411d124c2cc1a7f748af8c90f88
Sapper609 <egilchrist0110@gmail.com>
2022-04-20 16:10:26 -0600

    fixed import to be system

--- root-config/src/mfe-root-config.js
+++ root-config/src/mfe-root-config.js
@@ -3,9 +3,7 @@ import { registerApplication, start } from "single-spa";
 registerApplication({
   name: "@mfe/react",
   app: () =>
-    System.import(
-      "http://localhost:8082/mfe-react.js"
-    ),
+    System.import("@mfe/react"),
   activeWhen: ["/"],
 });
 
