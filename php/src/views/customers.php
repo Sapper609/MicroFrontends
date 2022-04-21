543ea30842477e30a6ed669276b8663d771c4427
Sapper609 <egilchrist0110@gmail.com>
2022-04-21 14:59:36 -0600

    Changed out default pages

--- php/src/views/customers.php
+++ php/src/views/customers.php
@@ -1,1 +1,5 @@
-<h1>customer</h1>
+<div style="text-align:center; padding:24px ">
+    <h1> The Customer Page </h1>
+    <p> this is the customer page in the PHP application </p>
+    <p> this is being displayed from the <b> customers.php </b> page in the php directory <p>
+</div>
